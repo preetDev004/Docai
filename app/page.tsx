9b6e29039709a39cbed3861c27b00ab6a46645a7
preet patel <preetpatelofficial004@gmail.com>
2024-08-22 12:42:35 +0530

    Tailwind Config with ShadcnUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center  text-black">
-      <div className="flex min-h-screen w-full max-w-[1200px] px-10 bg-white items-center justify-between">
-      <h1>Docai</h1>
-      <h1>App</h1>
+    <MaxWidthWrapper classname="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
+
+      {/* badge */}
+      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300  hover:bg-white/50">
+        <p className="text-sm font-semibold text-gray-700"><span className="text-blue-600">Docai</span> is now public!</p>
       </div>
-    </main>
+    </MaxWidthWrapper>
   );
 }
